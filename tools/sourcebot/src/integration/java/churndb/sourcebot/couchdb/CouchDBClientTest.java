6bae91779fb95af545e883bedbc2c37898b2cbfe
Fernando Ultremare <feroult@gmail.com>
2013-04-17 22:56:25 -0300

    couchdb client

--- tools/sourcebot/src/integration/java/churndb/sourcebot/couchdb/CouchDBClientTest.java
+++ tools/sourcebot/src/integration/java/churndb/sourcebot/couchdb/CouchDBClientTest.java
@@ -7,10 +7,16 @@ import com.google.gson.JsonObject;
 
 public class CouchDBClientTest {
 
+	private static final String COUCHDB_HOST = "http://127.0.0.1:5984";
+
 	@Test
 	public void testVersion() {
 		CouchDBClient chouch = new CouchDBClient();
-		JsonObject welcome = chouch.get("http://127.0.0.1:5984");
+		JsonObject welcome = chouch.get(COUCHDB_HOST);		
 		Assert.assertEquals("Welcome", welcome.get("couchdb").getAsString());
+	}
+	
+	public void testCreateDrop() {		
+		// TODO	
 	}
 }
