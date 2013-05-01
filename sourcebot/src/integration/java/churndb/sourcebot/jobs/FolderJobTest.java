c3d25c95522bd221700f01dc089208d7a5f3b803
Fernando Ultremare <feroult@gmail.com>
2013-05-01 16:35:28 -0300

    load from svn importing to couch

--- sourcebot/src/integration/java/churndb/sourcebot/jobs/FolderJobTest.java
+++ sourcebot/src/integration/java/churndb/sourcebot/jobs/FolderJobTest.java
@@ -9,7 +9,6 @@ import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import churndb.sourcebot.SourceBot;
