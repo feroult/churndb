0883370cb2c938a6ee6b2d274fb576b7ba16041e
Fernando Ultremare <feroult@gmail.com>
2013-05-01 16:50:04 -0300

    refactoring packages

---
+++ sourcebot/src/integration/java/churndb/sourcebot/couchdb/DatabaseTest.java
@@ -0,0 +1,32 @@
+package churndb.sourcebot.couchdb;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import churndb.sourcebot.couchdb.response.CouchResponse;
+
+import com.google.gson.JsonObject;
+
+public class DatabaseTest extends CouchTestBase {
+
+	@Test
+	public void testWelcome() {
+		JsonObject welcome = couch.welcome().json();
+		Assert.assertEquals("Welcome", welcome.get("couchdb").getAsString());
+	}
+		
+	@Test
+	public void testCreateDropDatabase() {
+		couch.dropIfExists();
+		couch.create();
+	
+		JsonObject info = couch.get().json();
+		Assert.assertEquals(CHURNDB, info.get("db_name").getAsString());
+	
+		couch.drop();
+		
+		CouchResponse response = couch.get(CHURNDB);
+		Assert.assertTrue(response.objectNotFound());
+	}
+
+}
