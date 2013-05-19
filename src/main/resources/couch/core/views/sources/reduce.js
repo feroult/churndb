c87999fc4c84ddeaa03b732a4a473e7b32646d79
Fernando Ultremare <feroult@gmail.com>
2013-05-19 20:05:38 -0300

    reduce with averages

--- src/main/resources/couch/core/views/sources/reduce.js
+++ src/main/resources/couch/core/views/sources/reduce.js
@@ -1,4 +1,4 @@
-function(keys, values, rereduce) {
+/*function(keys, values, rereduce) {
   var reduce = {'CCN': 0, 'LOC': 0};
 
   for(var i = 0; i < values.length; i++) {
@@ -8,4 +8,45 @@ function(keys, values, rereduce) {
   }
   
   return reduce;
+}*/
+
+function(keys, values, rereduce) {	
+	if(rereduce) {		
+		var result = {};		
+		for( var metricKey in values[0] ) {
+			result[metricKey] = {
+				sum: values[0][metricKey].sum,
+				count: values[0][metricKey].count
+			}			
+				
+			for(var i = 1, l=values.length; i < l; ++i) {
+					result[metricKey].sum = result[metricKey].sum + values[i][metricKey].sum;
+		            result[metricKey].count = result[metricKey].count + values[i][metricKey].count;	        
+			}
+		
+			result[metricKey].avg = (result[metricKey].sum / result[metricKey].count);
+        }
+
+		log('rereduce keys:'+toJSON(keys)+' values:'+toJSON(values)+' result:'+toJSON(result));	    
+		return result;		
+	}
+	
+	// non-rereduce
+	var result = {};		
+	for( var metricKey in values[0].metrics ) {		
+		result[metricKey] = {
+			sum: values[0].metrics[metricKey],
+			count: 1
+		}			
+			
+		for(var i = 1, l=values.length; i < l; ++i) {
+				result[metricKey].sum = result[metricKey].sum + values[i].metrics[metricKey];
+	            result[metricKey].count = result[metricKey].count + 1;	        
+		}
+	
+		result[metricKey].avg = (result[metricKey].sum / result[metricKey].count);
+	}
+	
+	log('reduce keys:'+toJSON(keys)+' values:'+toJSON(values)+' result:'+toJSON(result));    
+	return result;
 }
