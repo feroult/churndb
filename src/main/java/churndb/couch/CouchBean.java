e3d2ce87f5663f855232dbefcb4454f941a73e62
Fernando Ultremare <feroult@gmail.com>
2013-05-31 18:08:26 -0300

    rethinking model

---
+++ src/main/java/churndb/couch/CouchBean.java
@@ -0,0 +1,30 @@
+package churndb.couch;
+
+import com.google.gson.Gson;
+
+public class CouchBean {
+
+	private String _id;
+
+	private String _rev;
+
+	public String get_id() {
+		return _id;
+	}
+
+	public void set_id(String _id) {
+		this._id = _id;
+	}
+
+	public String get_rev() {
+		return _rev;
+	}
+
+	public void set_rev(String _rev) {
+		this._rev = _rev;
+	}
+
+	public String json() {
+		return new Gson().toJson(this);
+	}
+}
