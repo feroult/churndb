e1838434bc8e681297b2e55712912619539a9375
Fernando Ultremare <feroult@gmail.com>
2013-06-04 19:14:42 -0300

    rename accross commits

--- src/test/resources/churndb/project/commit4/AddressRename.java
+++ src/test/resources/churndb/project/commit4/AddressRename.java
@@ -1,48 +1,14 @@
-package churndb.git;
+package churndb.project;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+public class AddressRename {
 
-public class Commit {
+	private String street;
 	
-	private Date date;
-	
-	List<Change> changes = new ArrayList<Change>();
-
-	private String name;
-
-	public Commit(Date date, String name) {
-		this.date = date;
-		this.name = name;		
+	public void setStreet(String street) {
+		this.street = street;
 	}
 	
-	public Date getDate() {
-		return date;
-	}
-
-	public void add(Type type, String oldPath, String newPath) {
-		changes.add(new Change(type, oldPath, newPath));
-	}
-
-	public List<Change> getChanges() {
-		return changes;
-	}
-
-	public Map<String, Change> getChangesAsMap() {
-		Map<String, Change> map = new HashMap<String, Change>();
-		
-		for(Change change : changes) {
-			map.put(change.getPath(), change);
-		}
-		
-		return map;
-	}
-
-	public String getName() {
-		return name;
-	}			
-
+	public void getStreet() {
+		return street;
+	}	
 }
