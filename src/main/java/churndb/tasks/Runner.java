97923747dee359e30be58d3d6c1102cf1b7b77c4
Fernando Ultremare <feroult@gmail.com>
2013-06-13 17:39:38 -0300

    runner

--- src/main/java/churndb/tasks/Runner.java
+++ src/main/java/churndb/tasks/Runner.java
@@ -1,82 +1,161 @@
 package churndb.tasks;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 
 public class Runner {
 
-	private static Map<String, Map<String, Method>> tasks;
-
-	private static Help help = new Help();
-
 	private static String prefix = "mvn exec:java ";
 
+	private Map<String, Class<? extends Task>> tasks;
+
+	private Map<String, Map<String, Method>> tasksMethods;
+
+	private Help help = new Help();
+
+	private Task task;
+
+	private Method method;
+
+	private List<Object> parameters;
+
 	public static void main(String... args) {
-		setupTasks();
+		Runner runner = new Runner();
+		runner.run(args);
+	}
+
+	private void run(String... args) {
+		setup();
 
 		if (isShowHelp(args)) {
 			showHelp();
-			return;
+			fail();
 		}
 
-		run(args);
-
+		if (initArgs(args)) {
+			exec(args);
+		}
 	}
 
-	private static void run(String... args) {
+	private boolean initArgs(String... args) {
+		String taskCmd = args[0];
 
-		Method taskMethod = getTaskMethod(args[0]);
-
-		
-		
-
-	}
-
-	private static Method getTaskMethod(String taskCmd) {
 		if (StringUtils.countMatches(taskCmd, ":") != 1) {
 			help.println("command {0} is not in the right format task:method", taskCmd);
-			fail();
+			return false;
 		}
 
 		String[] split = taskCmd.split(":");
 		String taskKey = split[0];
 		String methodKey = split[1];
 
-		if (!tasks.containsKey(taskKey)) {
-			help.println("task {0} does not exist", taskKey);
-			fail();
+		if (!initTask(taskKey)) {
+			return false;
 		}
 
-		Map<String, Method> taskMethods = tasks.get(taskKey);
+		if (!initMethod(taskKey, methodKey)) {
+			return false;
+		}
+
+		if (!initParameters(ArrayUtils.remove(args, 0))) {
+			return false;
+		}
+
+		return true;
+	}
+
+	private boolean initTask(String taskKey) {
+		if (!tasks.containsKey(taskKey)) {
+			help.println("task {0} does not exist", taskKey);
+			return false;
+		}
+
+		task = getTask(taskKey);
+		return true;
+	}
+
+	private Task getTask(String taskKey) {
+		try {
+			return tasks.get(taskKey).newInstance();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private void exec(String... args) {
+		try {
+			method.invoke(task, parameters.toArray());
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private boolean initParameters(String[] parametersAsString) {
+
+		if (method.getParameterTypes().length != parametersAsString.length) {
+			help.println("wrong number of parameters for method {0}", getTaskMethodKey(method));
+			return false;
+		}
+
+		parameters = new ArrayList<Object>();
+
+		Class<?>[] parameterTypes = method.getParameterTypes();
+
+		for (int i = 0; i < parametersAsString.length; i++) {
+			parameters.add(getMethodParameter(parametersAsString[i], parameterTypes[i]));
+		}
+
+		return true;
+	}
+
+	private Object getMethodParameter(String parameterAsString, Class<?> parameterType) {
+		try {
+			Constructor<?> constructor = parameterType.getConstructor(String.class);
+			return constructor.newInstance(parameterAsString);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private boolean initMethod(String taskKey, String methodKey) {
+		Map<String, Method> taskMethods = tasksMethods.get(taskKey);
 
 		if (!taskMethods.containsKey(methodKey)) {
 			help.println("task {0} does not contain method {1}", taskKey, methodKey);
-			fail();
+			return false;
 		}
 
-		return taskMethods.get(methodKey);
+		method = taskMethods.get(methodKey);
+		return true;
 	}
 
-	private static void fail() {
+	private void fail() {
 		System.exit(-1);
 	}
 
-	private static boolean isShowHelp(String... args) {
+	private boolean isShowHelp(String... args) {
 		return args.length == 0 || args[0].equalsIgnoreCase("-h");
 	}
 
-	private static void setupTasks() {
-		tasks = new TreeMap<String, Map<String, Method>>();
+	private void setup() {
+		tasks = new TreeMap<String, Class<? extends Task>>();
+		tasksMethods = new TreeMap<String, Map<String, Method>>();
 
 		addTask(ApplicationTask.class);
 		addTask(ProjectTask.class);
 	}
 
-	private static void addTask(Class<? extends Task> task) {
+	private void addTask(Class<? extends Task> task) {
+		tasks.put(getTaskKey(task), task);
+
 		Map<String, Method> taskMethods = new TreeMap<String, Method>();
 
 		for (Method method : task.getDeclaredMethods()) {
@@ -87,17 +166,17 @@ public class Runner {
 			taskMethods.put(getTaskMethodKey(method), method);
 		}
 
-		tasks.put(getTaskKey(task), taskMethods);
+		tasksMethods.put(getTaskKey(task), taskMethods);
 	}
 
-	public static void showHelp() {
-		for (String taskKey : tasks.keySet()) {
+	private void showHelp() {
+		for (String taskKey : tasksMethods.keySet()) {
 			listTask(taskKey);
 		}
 	}
 
-	private static void listTask(String taskKey) {
-		Map<String, Method> taskMethods = tasks.get(taskKey);
+	private void listTask(String taskKey) {
+		Map<String, Method> taskMethods = tasksMethods.get(taskKey);
 
 		for (String methodKey : taskMethods.keySet()) {
 			Method method = taskMethods.get(methodKey);
@@ -110,7 +189,7 @@ public class Runner {
 		}
 	}
 
-	private static String getTaskMethodHelp(Method method) {
+	private String getTaskMethodHelp(Method method) {
 		RunnerHelp runnerHelp = method.getAnnotation(RunnerHelp.class);
 
 		if (runnerHelp == null || runnerHelp.value().equals("")) {
@@ -120,11 +199,11 @@ public class Runner {
 		return " " + runnerHelp.value();
 	}
 
-	private static String getTaskMethodKey(Method method) {
+	private String getTaskMethodKey(Method method) {
 		return StringUtils.uncapitalize(method.getName());
 	}
 
-	private static String getTaskKey(Class<? extends Task> task) {
+	private String getTaskKey(Class<? extends Task> task) {
 		return StringUtils.uncapitalize(task.getSimpleName().replaceAll("Task", ""));
 	}
 }
