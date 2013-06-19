27fe4cc02507bd7c38737fc1269caeb5a0b9cbab
Fernando Ultremare <feroult@gmail.com>
2013-06-19 16:23:59 -0300

    commit tree

---
+++ src/main/resources/churn/trees/map/sources.js
@@ -0,0 +1,10 @@
+function(doc) {
+  if(doc.type && doc.type == 'tree') {
+    for(var i = 0; i < doc.sources.length; i++) {
+	    var source = doc.sources[i];
+        emit([doc.projectCode, 
+              doc.commit], 
+              {_id: source});
+    }
+  }
+}
