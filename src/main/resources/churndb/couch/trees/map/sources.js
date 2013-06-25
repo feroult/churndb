1040c947ba4722bce4489f43864821decd78a3af
Fernando Ultremare <feroult@gmail.com>
2013-06-24 23:40:29 -0300

    churn graph

--- src/main/resources/churndb/couch/trees/map/sources.js
+++ src/main/resources/churndb/couch/trees/map/sources.js
@@ -2,9 +2,8 @@ function(doc) {
   if(doc.type && doc.type == 'tree') {
     for(var i = 0; i < doc.sources.length; i++) {
 	    var source = doc.sources[i];
-        emit([doc.projectCode, 
-              doc.commit], 
-              {_id: source});
+        emit(doc.commit, 
+             {_id: source});
     }
   }
 }
